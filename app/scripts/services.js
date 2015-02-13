bd8069eb3ea443b5a5ffd8ab42be522db06241ab
unknown <sudhan.k.ramachandra@BDC6-L-4230Q4R.GDNINDIA.COM>
2015-02-13 12:25:54 +0530

    logout bug fix

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -6,7 +6,9 @@ var bettingAppServices = angular.module('bettingAppServices', []);
 bettingAppServices.service("sessionCheck", function($location){
 	return function(){
 		if(!sessionStorage.employeeId){
-			$location.path("/login");
+			if($location.path() != "/register"){
+				$location.path("/login");
+			}
 		}else{
 			$location.path("/dashboard");
 		}
